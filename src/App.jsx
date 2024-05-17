476b3e5cd0222ec5737ed4de15db949644cdf7ec
Art <honeydewart@gmail.com>
2024-05-18 00:03:32 +0800

    added query and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,53 @@
-import React from 'react';
-import { useQuery, useMutation } from '@tanstack/react-query';
+import { useQueryClient } from "@tanstack/react-query"
+import { useState } from "react"
+import { getPost } from "./api/posts"
+import { CreatePost } from "./CreatePost"
+import Post from "./Post"
+import { PostListInfinite } from "./PostListInfinite"
+import { PostListPaginated } from "./PostListPaginated"
+import PostsList1 from "./PostsList1"
+import PostsList2 from "./PostsList2"
 
+export default function App() {
+  const [currentPage, setCurrentPage] = useState(<PostsList1 />)
+  const queryClient = useQueryClient()
 
-const POSTS = [
-  { id: 1, title: 'Post 1' },
-  { id: 2, title: 'Post 2' },
-]
-
-function App() {
-  const postsQuery = useQuery({
-    queryKey: ['posts'],
-    queryFn: () =>
-      Promise.reject("Error"),
-  })
-
-  if (postsQuery.isLoading) {
-    return <h1>Loading...</h1>
-  } else if (postsQuery.isError) {
-    return <pre>{JSON.stringify(postsQuery.error)}</pre>
+  function onHoverPostOneLink() {
+    queryClient.prefetchQuery({
+      queryKey: ["posts", 1],
+      queryFn: () => getPost(1),
+    })
   }
 
-
   return (
-    <h1>Tanstack Query</h1>
+    <div>
+      <button onClick={() => setCurrentPage(<PostsList1 />)}>
+        Posts List 1
+      </button>
+      <button onClick={() => setCurrentPage(<PostsList2 />)}>
+        Posts List 2
+      </button>
+      <button
+        onMouseEnter={onHoverPostOneLink}
+        onClick={() => setCurrentPage(<Post id={1} />)}
+      >
+        First Post
+      </button>
+      <button
+        onClick={() =>
+          setCurrentPage(<CreatePost setCurrentPage={setCurrentPage} />)
+        }
+      >
+        New Post
+      </button>
+      <button onClick={() => setCurrentPage(<PostListPaginated />)}>
+        Post List Paginated
+      </button>
+      <button onClick={() => setCurrentPage(<PostListInfinite />)}>
+        Post List Infinite
+      </button>
+      <br />
+      {currentPage}
+    </div>
   )
 }
-
-
-function wait(duration) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, duration)
-  })
-}
-
-export default App;
